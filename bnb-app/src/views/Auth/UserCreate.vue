f3901fdb5f5f6ff83b90934a7d3e6a3c2aeb1e74
cesar-augt <79664244+cesar-augt@users.noreply.github.com>
2024-04-01 02:38:14 -0300

    Update UserCreate.vue

--- bnb-app/src/views/Auth/UserCreate.vue
+++ bnb-app/src/views/Auth/UserCreate.vue
@@ -5,10 +5,10 @@
     </v-toolbar>
     <v-card-text>
       <v-form @submit.prevent="login">
-        <v-text-field v-model="username" label="Username" outlined required></v-text-field>
-        <v-text-field v-model="email" label="Email" outlined required></v-text-field>
-        <v-text-field v-model="password" label="Password" outlined required type="password"></v-text-field>
-        <v-btn color="blue-lighten-3" dark block type="submit">Login</v-btn>
+        <v-text-field v-model="username" :error-messages="errors && errors.name" label="Username" outlined required></v-text-field>
+        <v-text-field v-model="email"  :error-messages="errors && errors.email" label="Email" outlined required></v-text-field>
+        <v-text-field v-model="password" :error-messages="errors && errors.password" label="Password" outlined required type="password"></v-text-field>
+        <v-btn color="blue-lighten-3" dark block type="submit">ADD USER</v-btn>
       </v-form>
     </v-card-text>
   </v-card>
@@ -24,20 +24,26 @@
         username : '',
         email: '', 
         password: '',
+        errors: []
       };
     },
     methods: {
       async login() {
         try {
-          await axios.post('/login/store', { email: this.email, password: this.password, name: this.username  }).then(response => {
-            this.$router.push('/login');
-            })
-            .catch(error => {
-              console.error('Erro ao fazer a requisição:', error);
+          const response = await axios.post('/login/store', { email: this.email, password: this.password, name: this.username  })
+            .then(function (response) {
+              return response
+            }).catch(error => {
+              console.error(error);
             });
-          this.$router.push('/home');
+          if(response.status === 422 ){  
+            this.errors = response.data.errors  
+          }
+          if(response.status === 200){
+            this.$router.push('/');
+          } 
         } catch (error) {
-          console.error('Error logging in:', error);
+          console.error(error);
         }
       },
     },
