c5b209812b49ac056e61184e6a5638b1f5d01163
cesar-augt <79664244+cesar-augt@users.noreply.github.com>
2024-04-01 02:39:27 -0300

    Update DepositCheck.vue

--- bnb-app/src/views/Customer/DepositCheck.vue
+++ bnb-app/src/views/Customer/DepositCheck.vue
@@ -3,12 +3,23 @@
     <v-toolbar color="blue-lighten-3" dark flat>
       <v-toolbar-title>CHECK DEPOSIT</v-toolbar-title>
     </v-toolbar>
+    <v-toolbar color="blue-lighten-3" dark flat>
+      <v-container>
+        <v-row>
+          <v-col cols="3">
+            <v-text-field v-model="total" type="text" label="Current Balance"  v-mask="'$###.###,##'"  readonly outlined > </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-toolbar>
     <v-card-text>
       <v-form @submit.prevent="submit">
-        <v-text-field v-model="description" label="Descrition" outlined required></v-text-field>
-        <v-text-field v-model="amount" label="Amount" outlined required v-mask="'$###.###,##'"></v-text-field>
-        <div>
-          <v-file-input
+        <v-text-field v-model="amount" :error-messages="errors.amount" label="Amount" outlined required v-mask="'$###.###,##'" hint="The money will be deposited in your account once the check is accepted">
+        </v-text-field>
+        <v-text-field v-model="description"  :error-messages="errors.description" label="Descrition" outlined required>
+        </v-text-field>
+          <v-file-input 
+            :error-messages="errors.check_file" 
             v-model="selectedFile.name"
             accept="image/*"
             label="Escolha uma imagem"
@@ -16,8 +27,7 @@
             prepend-icon="mdi-camera"
             @change="handleImageUpload"
           ></v-file-input>
-        </div>
-        <v-btn color="blue-lighten-3" dark block type="submit">ADD DEPOSIT</v-btn>
+        <v-btn color="blue-lighten-3"  :loading="loading" dark block type="submit">ADD DEPOSIT</v-btn>
       </v-form>
     </v-card-text>
   </v-card>
@@ -25,7 +35,7 @@
 </template>
   
   <script>
-  import axios from '../../service/auth';
+import axios from '../../service/auth';
   
   export default {
     data() {
@@ -35,16 +45,26 @@
         date : '',
         selectedFile: {
           name:''
-        }
+        },
+        errors: [],
+        loading: false,
+        total: 0
       };
     },
+    mounted() {
+      this.getData()
+    },
     methods: {
+      getData() {
+        this.total = this.$store.state.balance.current
+      },
       async submit() {
         try {
-          this.uploadImage()
-          this.$router.push('/home');
+          this.loading = true
+          await this.uploadImage()
+          this.loading = false
         } catch (error) {
-          console.error('Error logging in:', error);
+          console.error(error);
         }
       },
       handleImageUpload(event) {
@@ -55,22 +75,30 @@
           alert('Por favor, selecione um arquivo.');
           return;
         }
-        const formData = new FormData();
-        formData.append('foto', this.selectedFile, this.selectedFile.name);
+        const formData = new FormData(); 
+        if(this.selectedFile.name != ''){
+          formData.append('check_file', this.selectedFile, this.selectedFile.name);
+        }
         formData.append('amount', this.amount.replace("$", ""));
         formData.append('description', this.description);
-       
+
         try {
-
-          axios.post('/deposit/file', formData , {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }});
-
-          this.$router.push('/home');
-
+          const response = await axios.post('/deposit/file', formData , {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }}).then(function (response) {
+              return response
+            }).catch(function (error) {
+              console.log(error)
+            });
+            if(response.status === 422){
+              this.errors = response.data.errors  
+            }
+            if(response.status === 201){
+              this.$router.push('/home');
+            }  
         } catch (error) {
-          console.error('Erro ao enviar a imagem:', error);
+          console.error(error); 
         }
       }
     },
