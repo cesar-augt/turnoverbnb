fce143dd155e00e5a98cf99bcf79ff0fb2a05e01
cesar-augt <79664244+cesar-augt@users.noreply.github.com>
2024-04-01 02:40:05 -0300

    Update App.vue

--- bnb-app/src/App.vue
+++ bnb-app/src/App.vue
@@ -1,51 +1,51 @@
 <template>
     <v-app >
           <v-navigation-drawer color="blue-lighten-3" dark flat v-model="drawer" app>
-            <v-list>
+            <v-list class="text-left">
               <v-list-item link to="/home">
                 <v-list-item-content>
-                  <v-list-item-title>BALANCE</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-scale</v-icon> BALANCE</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>INCOMES</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-chevron-up</v-icon> INCOMES</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item link to="/expenses">
                 <v-list-item-content>
-                  <v-list-item-title>EXPENSES</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-chevron-down</v-icon> EXPENSES</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item link  to="/check">
                 <v-list-item-content>
-                  <v-list-item-title>CHECK</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-check</v-icon> CHECK</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+                <v-list-item link>
+                <v-list-item-content>
+                  <v-list-item-title><v-icon>mdi-bell</v-icon> NOTIFICATION</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>NOTIFICATION</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-account</v-icon> PROFILE</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>PROFILE</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-wrench</v-icon> SETTINGS</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-list-item link>
                 <v-list-item-content>
-                  <v-list-item-title>SETTINGS</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item link>
-                <v-list-item-content>
-                  <v-list-item-title>HELP</v-list-item-title>
+                  <v-list-item-title><v-icon>mdi-help</v-icon> HELP</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
             </v-list>
           </v-navigation-drawer>
           <v-app-bar  color="blue-lighten-3" dark flat app>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+              <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-btn icon @click="toggleTheme">
               <v-icon>{{ isDarkTheme ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent' }}</v-icon>
             </v-btn>
