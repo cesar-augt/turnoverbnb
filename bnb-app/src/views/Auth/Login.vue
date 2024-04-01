670cb8c674ad1a521c35acfe886f3c8e14431e58
cesar-augt <79664244+cesar-augt@users.noreply.github.com>
2024-04-01 02:37:58 -0300

    Update Login.vue

--- bnb-app/src/views/Auth/Login.vue
+++ bnb-app/src/views/Auth/Login.vue
@@ -4,28 +4,40 @@
       <v-toolbar-title>Login</v-toolbar-title>
     </v-toolbar>
     <v-card-text>
-      <v-form @submit.prevent="login">
+      <v-form @submit.prevent="submit">
         <v-container>
           <v-row>
-            <v-text-field v-model="email" label="Email" outlined required></v-text-field>
+            <v-text-field v-model="email" :error-messages="errors && errors.email"  label="Email" outlined ></v-text-field>
           </v-row>
           <v-row>
-            <v-text-field v-model="password" label="Password" outlined required type="password"></v-text-field>
+            <v-text-field v-model="password" label="Password" outlined :error-messages="errors && errors.password" type="password"></v-text-field>
           </v-row>
           <v-row>
             <v-col>
-              <v-btn color="blue-lighten-3" dark block type="submit">Login</v-btn>
-              </v-col>
-              <v-col>
-                <v-btn color="red" dark block type="button" @click="createUser" >Create a new user</v-btn>
+              <v-btn color="blue-lighten-3"  :loading="loading" dark block type="submit">Login</v-btn>
               </v-col>
             </v-row>
-        
+            <v-row>
+              <v-col>
+                <v-link class="elevation-12" dark flat @click="createUser"> Create a new user</v-link>
+              </v-col>
+            </v-row>
       </v-container>
       </v-form>
     </v-card-text>
   </v-card>
-      
+  <v-snackbar v-model="snackbar">
+      {{ snack_text }}
+      <template v-slot:actions>
+        <v-btn
+          color="red"
+          variant="text"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
 </template>
   
   <script>
@@ -36,25 +48,64 @@
       return {
         email: '', 
         password: '',
+        snackbar: false,
+        snack_text: '',
+        errors: []
       };
     },
     methods: {
+      async submit() {
+        try {
+          await this.login()
+          await this.isAdmin()
+        } catch (error) {
+          console.error(error);
+        }
+      },
       async login() {
         try {
-          await axios.post('/login', { email: this.email, password: this.password }).then(response => {
+          const response = await axios.post('/login', { email: this.email, password: this.password })
+            .then(function (response) {
               localStorage.setItem('token', response.data.access_token)
+              return response
             })
             .catch(error => {
               console.error('Erro ao fazer a requisição:', error);
             });
-          this.$router.push('/home');
+            if(response.status === 401){   
+              this.snackbar = true
+              this.snack_text = response.data.message  
+            }
+            if(response.status === 422){
+              this.errors = response.data.errors  
+            }
+            if(response.status === 200){
+              await this.isAdmin()
+              this.$router.push('/home');
+            }
         } catch (error) {
           console.error('Error logging in:', error);
         }
       },
-      async createUser(){
+      createUser(){
         try {
           this.$router.push('/user/create');
+        }catch (error) {
+          console.error(error)
+        }
+      },
+      async isAdmin(){
+        try {
+          const response = await axios.get('/login/admin')
+          .then(function (response) {
+           return response
+          })
+          .catch(error => {
+            console.error('Erro ao fazer a requisição:', error);
+          });
+          if(response.status === 200){
+            this.$router.push('/check/control');
+          }
         }catch (error) {
           console.error(error)
         }
