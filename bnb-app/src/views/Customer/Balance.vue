c390d2d99c156d18a1204a60bae5f866d9219a73
cesar-augt <79664244+cesar-augt@users.noreply.github.com>
2024-04-01 02:39:10 -0300

    Update Balance.vue

--- bnb-app/src/views/Customer/Balance.vue
+++ bnb-app/src/views/Customer/Balance.vue
@@ -1,5 +1,10 @@
 <template>
   <v-card >
+    <v-app-bar color="blue-lighten-3" dark flat app>
+            <v-container  justify-center>
+              <v-app-title >BNB Bank</v-app-title>
+            </v-container>
+          </v-app-bar>
     <v-toolbar color="blue-lighten-3" extended  dark flat>
       <v-container>
         <v-row>
@@ -22,7 +27,7 @@
         <v-container>
           <v-row>
               <v-col>
-                <v-text-field v-model="total_purchases" type="text" label="Incomes" readonly outlined  v-mask="'$###.###,##'"></v-text-field>
+                <v-text-field v-model="total_deposits" type="text" label="Incomes" readonly outlined  v-mask="'$###.###,##'"></v-text-field>
               </v-col>
               <v-col>
                 <v-card @click="openDeposit">
@@ -37,7 +42,7 @@
           </v-row>
           <v-row>
               <v-col>
-                <v-text-field v-model="total_deposits" type="text" label="Expenses" v-mask="'$###.###,##'" readonly outlined ></v-text-field>
+                <v-text-field v-model="total_purchases" type="text" label="Expenses" v-mask="'$###.###,##'" readonly outlined ></v-text-field>
               </v-col>
               <v-col>
                 <v-card @click="openPurchase">
@@ -55,7 +60,7 @@
     </v-card-text>
     <v-card-text>
       <v-form>
-            <div class="text-left font-weight-bold">TRANSACTIONS</div>
+        <div class="text-left font-weight-bold">TRANSACTIONS</div>
         <ListCard v-for="(data, index) in balance" :key="index" :card="data" />
       </v-form>
     </v-card-text>
@@ -75,23 +80,30 @@ export default {
       total: 0,
       total_purchases: 0,
       total_deposits: 0,
-      selectedDate : '2024-03',
+      selectedDate : '',
       balance: null,
     }
   },
   mounted() {
+    this.getYearAndMonth()
     this.getData()
   },
   methods: {
+    getYearAndMonth(){
+      const date = new Date()
+      this.selectedDate =  date.getFullYear()
+      this.selectedDate += "-" + (date.getMonth() + 1).toString().padStart(2, "0")
+    },
     async getData() {
       try {
         const date = this.selectedDate.split('-')
         if(date[0] >= 2020){
-          const response = await axios.get('http://127.0.0.1:8000/api/balance/' + date[1] + '/' + date[0])
+          const response = await axios.get('/balance/' + date[1] + '/' + date[0])
           this.balance = response.data.balance
           this.total_purchases = response.data.total_purchases
           this.total_deposits = response.data.total_deposits
           this.total = this.total_deposits - this.total_purchases
+          this.$store.commit('setBalance', { current: this.total })
         }
       } catch (error) {
         console.error(error)
