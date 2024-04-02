0c606067b9bb820c1d2da1819b5e0d3524fec341
cesar-augt <79664244+cesar-augt@users.noreply.github.com>
2024-04-02 09:18:16 -0300

    Update UserCreate.vue

--- bnb-app/src/views/Auth/UserCreate.vue
+++ bnb-app/src/views/Auth/UserCreate.vue
@@ -1,14 +1,32 @@
 <template>
   <v-card class="elevation-12">
     <v-toolbar color="blue-lighten-3" dark flat>
-      <v-toolbar-title>Login</v-toolbar-title>
+      <v-toolbar-title>NEW ACCOUNT</v-toolbar-title>
     </v-toolbar>
     <v-card-text>
       <v-form @submit.prevent="login">
-        <v-text-field v-model="username" :error-messages="errors && errors.name" label="Username" outlined required></v-text-field>
-        <v-text-field v-model="email"  :error-messages="errors && errors.email" label="Email" outlined required></v-text-field>
-        <v-text-field v-model="password" :error-messages="errors && errors.password" label="Password" outlined required type="password"></v-text-field>
-        <v-btn color="blue-lighten-3" dark block type="submit">ADD USER</v-btn>
+        <v-container>
+          <v-row justify="center">
+            <v-col lg="6" >
+              <v-text-field v-model="username" :error-messages="errors && errors.name" label="Username" outlined required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row justify="center">
+            <v-col lg="6" >
+              <v-text-field v-model="email"  :error-messages="errors && errors.email" label="Email" outlined required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row justify="center">
+            <v-col lg="6" >
+              <v-text-field v-model="password" :error-messages="errors && errors.password" label="Password" outlined required type="password"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row justify="center">
+            <v-col lg="6" >
+              <v-btn color="blue-lighten-3" dark block type="submit">ADD USER</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-form>
     </v-card-text>
   </v-card>
